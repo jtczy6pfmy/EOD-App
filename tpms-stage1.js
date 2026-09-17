8b3be0855ebeb827620b6af64830fd150eb30963
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-17 18:12:13 -0400

    Correct TPMS cloud URL to Goodyear Mobility Cloud

--- tpms-stage1.js
+++ tpms-stage1.js
@@ -3,7 +3,7 @@
   const SOURCE="https://raw.githubusercontent.com/jtczy6pfmy/EOD-App/802834bbbdcf60bdf4e88772e9d20b4d047b16af/tpms-stage1.js";
   fetch(SOURCE).then(r=>r.text()).then(src=>{
     const oldUrl="https://tiretrac.ap.goodyear.com/WebApp/Membership/Login.aspx?ReturnUrl=%2F";
-    const newUrl="https://goodyearmobilecloud.com";
+    const newUrl="https://goodyearmobilitycloud.com";
     if(!src.includes(oldUrl)) throw new Error("TPMS source URL not found");
     (0,eval)(src.replace(oldUrl,newUrl));
   }).catch(err=>console.error("EOD TPMS module failed to load",err));

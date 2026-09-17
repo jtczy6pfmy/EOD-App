3a526ace54e9ad269c3c130fa8779160e88a93bf
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-17 16:37:03 -0400

    Add TPMS flow compatibility hook

---
+++ tpms-flow-fix.js
@@ -0,0 +1,5 @@
+(()=>{
+  "use strict";
+  // Compatibility hook for the TPMS workflow loader.
+  // The active TPMS layout and pre-inspection confirmation are installed by tpms-enhancement.js.
+})();

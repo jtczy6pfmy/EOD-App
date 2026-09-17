9b91a28a4ee687fb924f3273e8cb76f80a2e5af7
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-17 19:29:55 -0400

    Remove TPMS flow fix module

--- tpms-flow-fix.js
+++
@@ -1,5 +0,0 @@
-(()=>{
-  "use strict";
-  // Compatibility hook for the TPMS workflow loader.
-  // The active TPMS layout and pre-inspection confirmation are installed by tpms-enhancement.js.
-})();
